c783119a5c52978ffe395bb5608d0b8111db223f
Jimxx22 <jaumegil22@gmail.com>
2022-07-29 12:08:01 +0200

    Control de errores

--- UD19-4/src/AppGrafica.java
+++ UD19-4/src/AppGrafica.java
@@ -11,6 +11,7 @@ import javax.swing.JOptionPane;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class AppGrafica {
 
@@ -43,38 +44,86 @@ public class AppGrafica {
 		result = new JTextField();
 		result.setEditable(false);
 		result.setColumns(10);
+		JLabel lblerror1 = new JLabel("");
+		lblerror1.setForeground(Color.RED);
+		
+		JLabel lblerror2 = new JLabel("");
+		lblerror2.setForeground(Color.RED);
 		
 		JButton sumaButton = new JButton("+");
 		sumaButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int operation = Integer.parseInt(num1.getText()) + Integer.parseInt(num2.getText());
-				result.setText(Integer.toString(operation));
+				
+				if(num1.getText().equals("")) {
+					lblerror1.setText("*Campo vacio");
+					lblerror2.setText("");
+				}else if (num2.getText().equals("")) {
+					lblerror2.setText("*Campo vacio");
+					lblerror1.setText("");
+				}else {
+					int operation = Integer.parseInt(num1.getText()) + Integer.parseInt(num2.getText());
+					result.setText(Integer.toString(operation));
+					lblerror1.setText("");
+					lblerror2.setText("");
+				}
+				
 			}
 		});
 		
 		JButton restaButton = new JButton("-");
 		restaButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int operation = Integer.parseInt(num1.getText()) - Integer.parseInt(num2.getText());
-				result.setText(Integer.toString(operation));
+				if(num1.getText().equals("")) {
+					lblerror1.setText("*Campo vacio");
+					lblerror2.setText("");
+				}else if (num2.getText().equals("")) {
+					lblerror2.setText("*Campo vacio");
+					lblerror1.setText("");
+				}else {
+					int operation = Integer.parseInt(num1.getText()) - Integer.parseInt(num2.getText());
+					result.setText(Integer.toString(operation));
+					lblerror1.setText("");
+					lblerror2.setText("");
+				}
 			}
 		});
 		
 		JButton multButton = new JButton("x");
 		multButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int operation = Integer.parseInt(num1.getText()) * Integer.parseInt(num2.getText());
-				result.setText(Integer.toString(operation));
-
+				if(num1.getText().equals("")) {
+					lblerror1.setText("*Campo vacio");
+					lblerror2.setText("");
+				}else if (num2.getText().equals("")) {
+					lblerror2.setText("*Campo vacio");
+					lblerror1.setText("");
+				}else {
+					int operation = Integer.parseInt(num1.getText()) * Integer.parseInt(num2.getText());
+					result.setText(Integer.toString(operation));
+					lblerror1.setText("");
+					lblerror2.setText("");
+				}
 			}
 		});
 		
 		JButton divideButton = new JButton("/");
 		divideButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int operation = Integer.parseInt(num1.getText()) / Integer.parseInt(num2.getText());
-				result.setText(Integer.toString(operation));
-
+				if(num1.getText().equals("")) {
+					lblerror1.setText("*Campo vacio");
+					lblerror2.setText("");
+				}else if (num2.getText().equals("")) {
+					lblerror2.setText("*Campo vacio");
+					lblerror1.setText("");
+				}else if(num2.getText().equals("0")){
+					lblerror2.setText("*No puede ser 0");
+					lblerror1.setText("");
+				}else {
+					int operation = Integer.parseInt(num1.getText()) / Integer.parseInt(num2.getText());
+					result.setText(Integer.toString(operation));
+					lblerror1.setText("");
+					lblerror2.setText("");
+				}
 			}
 		});
 		
@@ -98,6 +147,8 @@ public class AppGrafica {
 		JLabel lblNumero = new JLabel("Numero 2");
 		
 		JLabel lblResultado = new JLabel("Resultado");
+		
+		
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -105,36 +156,41 @@ public class AppGrafica {
 					.addGap(43)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(17)
-							.addComponent(lblNewLabel)
-							.addGap(54)
-							.addComponent(lblNumero)
-							.addGap(106)
-							.addComponent(lblResultado)
-							.addGap(76))
-						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-							.addComponent(aboutButton, Alignment.LEADING)
+							.addComponent(lblerror1)
+							.addContainerGap())
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(num1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.UNRELATED)
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addGroup(groupLayout.createSequentialGroup()
-										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-											.addComponent(sumaButton)
-											.addComponent(multButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE))
-										.addGap(57)
-										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-											.addComponent(divideButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
-											.addComponent(restaButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE))
-										.addGap(156))
-									.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-										.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-											.addComponent(exitButton)
-											.addGroup(groupLayout.createSequentialGroup()
-												.addComponent(num2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-												.addPreferredGap(ComponentPlacement.RELATED, 74, Short.MAX_VALUE)
-												.addComponent(result, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-										.addGap(51)))))))
+								.addGap(17)
+								.addComponent(lblNewLabel)
+								.addGap(54)
+								.addComponent(lblNumero)
+								.addGap(106)
+								.addComponent(lblResultado)
+								.addGap(76))
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(aboutButton, Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(num1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+										.addGroup(groupLayout.createSequentialGroup()
+											.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addComponent(sumaButton)
+												.addComponent(multButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE))
+											.addGap(57)
+											.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addComponent(divideButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+												.addComponent(restaButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE))
+											.addGap(156))
+										.addGroup(groupLayout.createSequentialGroup()
+											.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+												.addComponent(exitButton)
+												.addGroup(groupLayout.createSequentialGroup()
+													.addComponent(num2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+													.addPreferredGap(ComponentPlacement.RELATED, 74, Short.MAX_VALUE)
+													.addComponent(result, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+												.addComponent(lblerror2, Alignment.LEADING))
+											.addGap(51))))))))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -149,7 +205,11 @@ public class AppGrafica {
 						.addComponent(num1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(num2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(result, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(46)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblerror1)
+						.addComponent(lblerror2))
+					.addGap(26)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(restaButton)
 						.addComponent(sumaButton))
